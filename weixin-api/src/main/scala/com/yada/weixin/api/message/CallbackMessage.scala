c67199c9f8ce0b9c73140760c364292ea707455f
cuitaocrazy <cuitaocrazy@163.com>
2016-03-23 13:27:28 +0800

    重新定义消息

--- weixin-api/src/main/scala/com/yada/weixin/api/message/CallbackMessage.scala
+++ weixin-api/src/main/scala/com/yada/weixin/api/message/CallbackMessage.scala
@@ -1,8 +1,5 @@
 package com.yada.weixin.api.message
 
-import scala.util.Try
-import scala.xml.XML
-
 /**
   * Created by cuitao-pc on 16/3/8.
   */
@@ -10,210 +7,214 @@ import scala.xml.XML
 object CallbackMessage {
 
   object Names {
+    /**
+      * 接收方帐号
+      */
+    final val ToUserName = "ToUserName"
+    /**
+      * 发送方帐号
+      */
+    final val FromUserName = "FromUserName"
+    /**
+      * 消息创建时间 （整型）
+      */
+    final val CreateTime = "CreateTime"
+    /**
+      * 消息类型
+      */
+    final val MsgType = "MsgType"
+    /**
+      * 消息id，64位整型
+      */
+    final val MsgId = "MsgId"
 
-    object Comm {
-      final val ToUserName = "ToUserName"
-      final val FromUserName = "FromUserName"
-      final val CreateTime = "CreateTime"
-      final val MsgType = "MsgType"
-      final val MsgId = "MsgId"
+    /**
+      * 文本消息内容
+      */
+    final val Content = "Content"
+
+    /**
+      * 图片链接
+      */
+    final val PicUrl = "PicUrl"
+    /**
+      * 媒体id，可以调用多媒体文件下载接口拉取数据
+      */
+    final val MediaId = "MediaId"
+
+    /**
+      * 语音格式，如amr，speex等
+      */
+    final val Format = "Format"
+    /**
+      * 语音识别出的信息
+      */
+    final val Recognition = "Recognition"
+
+    /**
+      * 缩略图的媒体id
+      */
+    final val ThumbMediaId = "ThumbMediaId"
+
+    /**
+      * 地理位置维度
+      */
+    final val LocationX = "Location_X"
+    /**
+      * 地理位置经度
+      */
+    final val LocationY = "Location_Y"
+    /**
+      * 地图缩放大小
+      */
+    final val Scale = "Scale"
+    /**
+      * 地理位置信息
+      */
+    final val Label = "Label"
+
+    /**
+      * 消息标题
+      */
+    final val Title = "Title"
+    /**
+      * 消息描述
+      */
+    final val Description = "Description"
+    /**
+      * 消息链接
+      */
+    final val Url = "Url"
+
+    /**
+      * 事件类型
+      */
+    final val Event = "Event"
+    /**
+      * 事件KEY值
+      */
+    final val EventKey = "EventKey"
+    /**
+      * 二维码的ticket，可用来换取二维码图片
+      */
+    final val Ticket = "Ticket"
+    /**
+      * 地理位置纬度
+      */
+    final val Latitude = "Latitude"
+    /**
+      * 地理位置经度
+      */
+    final val Longitude = "Longitude"
+    /**
+      * 地理位置精度
+      */
+    final val Precision = "Precision"
+
+    final val Voice = "Voice"
+
+    final val Music = "Music"
+    /**
+      * 音乐链接
+      */
+    final val MusicUrl = "MusicUrl"
+    /**
+      * 高质量音乐链接，WIFI环境优先使用该链接播放音乐
+      */
+    final val HQMusicUrl = "HQMusicUrl"
+
+    /**
+      * 图文消息个数，限制为10条以内
+      */
+    final val ArticleCount = "ArticleCount"
+    /**
+      * 多条图文消息信息，默认第一个item为大图,注意，如果图文数超过10，则将会无响应
+      */
+    final val Articles = "Articles"
+    /**
+      * 项
+      */
+    final val Item = "item"
+
+    final val Status = "Status"
+    /**
+      * group_id下粉丝数；或者openid_list中的粉丝数
+      */
+    final val TotalCount = "TotalCount"
+    /**
+      * 过滤（过滤是指特定地区、性别的过滤、用户设置拒收的过滤，用户接收已超4条的过滤）后，准备发送的粉丝数，原则上，FilterCount = SentCount + ErrorCount
+      */
+    final val FilterCount = "FilterCount"
+    /**
+      * 发送成功的粉丝数
+      */
+    final val SentCount = "SentCount"
+    /**
+      * 发送失败的粉丝数
+      */
+    final val ErrorCount = "ErrorCount"
+
+    object MSG_TYPE {
+      final val Text = "text"
+      final val Image = "image"
+      final val Voice = "voice"
+      final val Video = "video"
+      final val ShortVideo = "shortvideo"
+      final val Location = "location"
+      final val link = "link"
+      final val Event = "event"
     }
 
-    object Text {
-      final val Content = "Content"
+    object EVENT_TYPE {
+      final val Subscribe = "subscribe"
+      final val UnSubscribe = "unsubscribe"
+      final val Scan = "SCAN"
+      final val Location = "LOCATION"
+      final val Click = "CLICK"
+      final val View = "VIEW"
+      final val MassSendJobFinish = "MASSSENDJOBFINISH"
     }
 
-    object Image {
-      final val PicUrl = "PicUrl"
-      final val MediaId = "MediaId"
+    object STATUS {
+      final val SendSuccess = "send success"
+      final val SendFail = "send fail"
+      /**
+        * 涉嫌广告
+        */
+      final val err_10001 = "err(10001)"
+      /**
+        * 涉嫌政治
+        */
+      final val err_20001 = "err(20001)"
+      /**
+        * 涉嫌色情
+        */
+      final val err_20002 = "err(20002)"
+      /**
+        * 涉嫌社会
+        */
+      final val err_20004 = "err(20004)"
+      /**
+        * 涉嫌违法犯罪
+        */
+      final val err_20006 = "err(20006)"
+      /**
+        * 涉嫌欺诈
+        */
+      final val err_20008 = "err(20008)"
+      /**
+        * 涉嫌版权
+        */
+      final val err_20013 = "err(20013)"
+      /**
+        * 涉嫌其他
+        */
+      final val err_21000 = "err(21000)"
+      /**
+        * 涉嫌互推(互相宣传)
+        */
+      final val err_22000 = "err(22000)"
     }
-
-    object Voice {
-      final val MediaId = "MediaId"
-      final val Format = "Format"
-      final val Recognition = "Recognition"
-    }
-
-    object Video {
-      final val MediaId = "MediaId"
-      final val ThumbMediaId = "ThumbMediaId"
-    }
-
-    object ShortVideo {
-      final val MediaId = "MediaId"
-      final val ThumbMediaId = "ThumbMediaId"
-    }
-
-    object Location {
-      final val LocationX = "Location_X"
-      final val LocationY = "Location_Y"
-      final val Scale = "Scale"
-      final val Label = "Label"
-    }
-
-    object Link {
-      final val Title = "Title"
-      final val Description = "Description"
-      final val Url = "Url"
-    }
-
   }
 
-  trait Message {
-    val commMessage: CommMessage
-
-    def toUserName = commMessage.toUserName
-
-    def fromUserName = commMessage.fromUserName
-
-    def createTime = commMessage.createTime
-
-    def msgType = commMessage.msgType
-
-    def msgId = commMessage.msgId
-  }
-
-  /**
-    * 公共信息
-    *
-    * @param toUserName   接收方
-    * @param fromUserName 发送方
-    * @param createTime   消息创建时间
-    * @param msgType      消息类型
-    * @param msgId        消息id
-    */
-  case class CommMessage(
-                          toUserName: String,
-                          fromUserName: String,
-                          createTime: String,
-                          msgType: String,
-                          msgId: String
-                        )
-
-  /**
-    * 文本消息
-    *
-    * @param commMessage 公共信息
-    * @param content     文本消息内容
-    */
-  case class TextMessage(
-                          commMessage: CommMessage,
-                          content: String
-                        ) extends Message
-
-  /**
-    * 图片消息
-    *
-    * @param commMessage 公共信息
-    * @param picUrl      图片链接
-    * @param mediaId     图片消息媒体id
-    */
-  case class ImageMessage(
-                           commMessage: CommMessage,
-                           picUrl: String,
-                           mediaId: String
-                         ) extends Message
-
-  /**
-    * 语音消息
-    *
-    * @param commMessage 公共信息
-    * @param mediaId     语音消息媒体id
-    * @param format      语音格式
-    */
-  case class VoiceMessage(
-                           commMessage: CommMessage,
-                           mediaId: String,
-                           format: String,
-                           recognition: String
-                         ) extends Message
-
-  /**
-    * 视频消息
-    *
-    * @param commMessage  公共信息
-    * @param mediaId      视频消息媒体id
-    * @param ThumbMediaId 视频消息缩略图的媒体id
-    */
-  case class VideoMessage(
-                           commMessage: CommMessage,
-                           mediaId: String,
-                           ThumbMediaId: String
-                         ) extends Message
-
-  /**
-    * 小视频消息
-    *
-    * @param commMessage  公共信息
-    * @param mediaId      视频消息媒体id
-    * @param ThumbMediaId 视频消息缩略图的媒体id
-    */
-  case class ShortVideoMessage(
-                                commMessage: CommMessage,
-                                mediaId: String,
-                                ThumbMediaId: String
-                              ) extends Message
-
-  /**
-    * 地理位置消息
-    *
-    * @param commMessage 公共信息
-    * @param locationX   地理位置维度
-    * @param locationY   地理位置经度
-    * @param scale       地图缩放大小
-    * @param label       地理位置信息
-    */
-  case class LocationMessage(
-                              commMessage: CommMessage,
-                              locationX: String,
-                              locationY: String,
-                              scale: String,
-                              label: String
-                            ) extends Message
-
-  /**
-    * 链接消息
-    *
-    * @param commMessage 公共信息
-    * @param Title       消息标题
-    * @param Description 消息描述
-    * @param Url         消息链接
-    */
-  case class LinkMessage(
-                          commMessage: CommMessage,
-                          Title: String,
-                          Description: String,
-                          Url: String
-                        ) extends Message
-
-  def strToMessage(strMsg: String): Try[Message] = Try {
-    val xml = XML.loadString(strMsg)
-    val comm = CommMessage(
-      (xml \ Names.Comm.ToUserName).head.text,
-      (xml \ Names.Comm.FromUserName).head.text,
-      (xml \ Names.Comm.CreateTime).head.text,
-      (xml \ Names.Comm.MsgType).head.text,
-      (xml \ Names.Comm.MsgId).head.text
-    )
-
-    comm.msgType match {
-      case "text" =>
-        TextMessage(comm, (xml \ Names.Text.Content).head.text)
-      case "image" =>
-        ImageMessage(comm, (xml \ Names.Image.PicUrl).head.text, (xml \ Names.Image.MediaId).head.text)
-      case "voice" =>
-        val recognitionNode = xml \ Names.Voice.Recognition
-        val recognition = if (recognitionNode.isEmpty) null else recognitionNode.head.text
-        VoiceMessage(comm, (xml \ Names.Voice.MediaId).head.text, (xml \ Names.Voice.Format).head.text, recognition)
-      case "video" =>
-        VideoMessage(comm, (xml \ Names.Video.MediaId).head.text, (xml \ Names.Video.ThumbMediaId).head.text)
-      case "shortvideo" =>
-        ShortVideoMessage(comm, (xml \ Names.ShortVideo.MediaId).head.text, (xml \ Names.ShortVideo.ThumbMediaId).head.text)
-      case "location" =>
-        LocationMessage(comm, (xml \ Names.Location.LocationX).head.text, (xml \ Names.Location.LocationY).head.text,
-          (xml \ Names.Location.Scale).head.text, (xml \ Names.Location.Label).head.text)
-      case "link" =>
-        LinkMessage(comm, (xml \ Names.Link.Title).head.text, (xml \ Names.Link.Description).head.text, (xml \ Names.Link.Url).head.text)
-    }
-  }
 }
