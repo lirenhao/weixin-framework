a5c809b6b13f6600f3b8961e1f4ab5fdb25c7488
cuitaocrazy <cuitaocrazy@163.com>
2016-03-28 10:46:14 +0800

    MessageProc加入到RoundRobinPoll,为加解密提供优化

--- weixin-api/src/main/scala/com/yada/weixin/cb/server/MessageProcActor.scala
+++ weixin-api/src/main/scala/com/yada/weixin/cb/server/MessageProcActor.scala
@@ -2,6 +2,7 @@ package com.yada.weixin.cb.server
 
 import akka.actor.{Actor, Props}
 import akka.pattern._
+import akka.routing.RoundRobinPool
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import com.yada.weixin.weixinExecutionContext
@@ -16,6 +17,7 @@ import scala.language.postfixOps
 class MessageProcActor extends Actor {
   private val messageProc = Class.forName(ConfigFactory.load().getString("weixin.callbackServer.messageProcClass"))
     .newInstance().asInstanceOf[MessageProc]
+
   override def receive: Receive = {
     case msg: String =>
       sender() ! messageProc.proc(msg)
@@ -24,7 +26,10 @@ class MessageProcActor extends Actor {
 
 // TODO: 超时时间? 如何设置或读取? 当前是1秒
 object MessageProcActor {
-  private val actor = com.yada.weixin.actorSystem.actorOf(Props[MessageProcActor], "MessageProcActor")
+  private val actor = com.yada.weixin.actorSystem.actorOf(
+    Props[MessageProcActor].withRouter(RoundRobinPool(Runtime.getRuntime.availableProcessors() * 2)),
+    "MessageProcActor")
+
   private implicit val timeout = Timeout(1 second)
 
   def procMsg(msg: String) = (actor ? msg).asInstanceOf[Future[Future[String]]].flatMap(f => f)
