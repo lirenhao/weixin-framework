e4693dbe55506fa772cce29ca44c7abbb38e309b
cuitaocrazy <cuitaocrazy@163.com>
2016-04-26 12:52:18 +0800

    修改MessageProc

--- weixin-api/src/main/scala/com/yada/weixin/cb/server/MessageProc.scala
+++ weixin-api/src/main/scala/com/yada/weixin/cb/server/MessageProc.scala
@@ -1,49 +1,37 @@
 package com.yada.weixin.cb.server
 
 import com.yada.weixin.api.message.CallbackMessage
+import play.api.libs.json.{JsValue, Json}
 
 import scala.concurrent.Future
-import scala.xml.{PCData, Utility, XML}
 
 /**
   * Created by Cuitao on 2016/3/6.
   */
-trait MessageProc {
-  /**
-    * 处理消息
-    *
-    * @param msg 请求的消息
-    * @return 未来相应的消息
-    */
-  def proc(msg: String): Future[String]
+trait MessageProc[T, U] {
+  val filter: JsValue => Boolean
+  val process: T => Future[U]
+  val requestCreator: JsValue => T
+  val responseCreator: (JsValue, U) => Option[JsValue]
+
+  def proc(jsValue: JsValue)(op: Option[JsValue] => String): Future[String] = {
+    process(requestCreator(jsValue)).map(m => op(responseCreator(jsValue, m)))
+  }
 }
 
-class SimpleMessageProc extends MessageProc {
-  override def proc(msg: String): Future[String] = {
-    Future.successful {
-      val xml = XML.loadString(msg)
-      val toUser = (xml \ CallbackMessage.Names.ToUserName).head.text
-      val fromUser = (xml \ CallbackMessage.Names.FromUserName).head.text
-      val content = (xml \ CallbackMessage.Names.Content).head.text
-      Utility.trim {
-        <xml>
-          <ToUserName>
-            {PCData(fromUser)}
-          </ToUserName>
-          <FromUserName>
-            {PCData(toUser)}
-          </FromUserName>
-          <CreateTime>
-            {System.currentTimeMillis() / 1000}
-          </CreateTime>
-          <MsgType>
-            {PCData("text")}
-          </MsgType>
-          <Content>
-            {PCData("你发的消息:" + content)}
-          </Content>
-        </xml>
-      }.toString()
-    }
+class SimpleMessageProc extends MessageProc[JsValue, String] {
+  override val filter: (JsValue) => Boolean = (jv) => (jv \ CallbackMessage.Names.MsgType).as[String] == CallbackMessage.Names.MSG_TYPE.Text
+  override val requestCreator: (JsValue) => JsValue = jv => jv
+  override val process: (JsValue) => Future[String] = req => Future.successful {
+    "you say:" + (req \ CallbackMessage.Names.Content).as[String]
+  }
+  override val responseCreator: (JsValue, String) => Option[JsValue] = (req, str) => Option {
+    Json.obj(
+      CallbackMessage.Names.ToUserName -> (req \ CallbackMessage.Names.FromUserName).as[String],
+      CallbackMessage.Names.FromUserName -> (req \ CallbackMessage.Names.ToUserName).as[String],
+      CallbackMessage.Names.CreateTime -> System.currentTimeMillis() / 1000,
+      CallbackMessage.Names.MsgType -> CallbackMessage.Names.MSG_TYPE.Text,
+      CallbackMessage.Names.Content -> str
+    )
   }
 }
--- weixin-api/src/main/scala/com/yada/weixin/cb/server/MessageProcActor.scala
+++ weixin-api/src/main/scala/com/yada/weixin/cb/server/MessageProcActor.scala
@@ -6,22 +6,36 @@ import akka.routing.RoundRobinPool
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import com.yada.weixin.weixinExecutionContext
+import org.json.XML
+import play.api.libs.json.{JsValue, Json}
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
+import collection.JavaConverters._
 
 /**
   * Created by cuitao on 2016/3/6.
   */
 class MessageProcActor extends Actor {
-  private val messageProc = Class.forName(ConfigFactory.load().getString("weixin.callbackServer.messageProcClass"))
-    .newInstance().asInstanceOf[MessageProc]
+  private val messageProcList = ConfigFactory.load().getStringList("weixin.callbackServer.messageProcClasses").asScala.map {
+    className =>
+      Class.forName(className).newInstance().asInstanceOf[MessageProc]
+  }
 
   override def receive: Receive = {
     case msg: String =>
-      Try (messageProc.proc(msg)) match {
+      Try {
+        val jv = (Json.parse(XML.toJSONObject(msg).toString()) \ "xml").as[JsValue]
+        messageProcList.find(mp => mp.filter(jv)) match {
+          case Some(mp) => mp.proc(jv) {
+            case Some(responseJv) => responseJv.toString()
+            case None => "success"
+          }
+          case None => Future.successful("success")
+        }
+      } match {
         case Success(f) => sender() ! f
         case Failure(e) => sender() ! Status.Failure(e)
       }
